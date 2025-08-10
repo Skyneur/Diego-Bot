1d88baea861359c44daf19dc31b1a7215a9717bf
Soren-git <soren.anth@gmail.com>
2025-08-10 18:52:51 +0200

    add: interaction manager

---
+++ src/events/interaction-create.ts
@@ -0,0 +1,16 @@
+import config from "@src/config";
+import { Event } from "@src/handlers/events";
+import { Client, Interaction } from "discord.js";
+import { Command } from "@src/handlers/commands";
+
+const event = new Event<[Client, Interaction]>(
+  "interactionCreate",
+  async (client, interaction) => {
+    if (interaction.isCommand()) {
+      let command = client.commands.get(interaction.commandName);
+      command.execute(client, interaction);
+    }
+  }
+);
+
+export default event;
