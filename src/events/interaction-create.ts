c3a55e5e3d3bbe4ee5d3a0e2a8185c06dafe846f
Soren-git <soren.anth@gmail.com>
2025-08-10 18:54:01 +0200

    del: unused import

--- src/events/interaction-create.ts
+++ src/events/interaction-create.ts
@@ -1,7 +1,5 @@
-import config from "@src/config";
 import { Event } from "@src/handlers/events";
 import { Client, Interaction } from "discord.js";
-import { Command } from "@src/handlers/commands";
 
 const event = new Event<[Client, Interaction]>(
   "interactionCreate",
